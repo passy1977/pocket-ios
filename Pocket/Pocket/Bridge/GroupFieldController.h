6b3c0ebdfaeca5f4a5ca5221488f55d3647a8c0e
Antonio Salsi <34831922+passy1977@users.noreply.github.com>
2025-10-27 06:19:13 +0100

    refactor: create new GroupFieldController

---
+++ Pocket/Pocket/Bridge/GroupFieldController.h
@@ -0,0 +1,52 @@
+/***************************************************************************
+ *
+ * Pocket
+ * Copyright (C) 2018/2025 Antonio Salsi <passy.linux@zresa.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+#ifdef __OBJC__
+#import <Foundation/Foundation.h>
+#endif
+
+#import "Globals.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class User;
+@class Group;
+@class GroupField;
+
+@interface GroupFieldController : NSObject
+
+//MARK: - System
+-(instancetype)init;
+-(void)initialize;
+
+//MARK: - GroupField
+-(uint32_t)getLastIdGroupField;
+
+//MARK: - Virtual list for handling new GroupField
+-(void)cleanShowList;
+-(void)fillShowList:(nonnull const Group *)group insert:(bool)insert;
+-(void)fillShowList:(nonnull const Group *)group;
+-(nonnull NSArray<GroupField*>*)getShowList;
+-(BOOL)addToShowList:(nonnull GroupField *)groupField;
+-(BOOL)delFromShowList:(uint32_t)idGroupField;
+-(uint8_t)sizeShowList;
+
+@end
+
+NS_ASSUME_NONNULL_END
